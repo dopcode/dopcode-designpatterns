6100b8ddc7c16e522c60bbad7446f062e256a1f2
dospluto <dospluto@gmail.com>
2020-10-17 05:54:15 +0900

    Source 반영

---
+++ src/main/java/Mediator/Colleague.java
@@ -0,0 +1,5 @@
+package Mediator;
+public interface Colleague {
+    public abstract void setMediator(Mediator mediator);
+    public abstract void setColleagueEnabled(boolean enabled);
+}
---
+++ src/main/java/Mediator/Mediator.java
@@ -0,0 +1,5 @@
+package Mediator;
+public interface Mediator {
+    public abstract void createColleagues();
+    public abstract void colleagueChanged();
+}
